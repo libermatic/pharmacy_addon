cafce2503fbf02b3e6662132e6476189e55d23d3
Sun Howwrongbum <sun@adrenosapient.com>
2017-12-05 16:33:50 +0530

    Add Pharmacy Settings doctype

---
+++ pharmacy_addon/pharmacy_addon/doctype/pharmacy_settings/pharmacy_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Libermatic and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Pharmacy Settings', {
+	refresh: function(frm) {
+
+	}
+});
